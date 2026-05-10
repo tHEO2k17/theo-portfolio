11fafc141c923c9ec4d4c85473f55f0f7fc668d3
Theophilus Paintsil <tpee935@gmail.com>
2026-05-10 13:59:39 +0000

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://theopaintsil.online/",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "weekly",
       priority: 1,
     },
   ];
